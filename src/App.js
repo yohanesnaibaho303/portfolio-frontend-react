792131117b11429cb831531726d933905e233bad
Yohanes Naibaho <yohanespratama303@gmail.com>
2023-06-02 20:46:28 +0700

    added navbar responsive and create structured files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
 
+import { About, Footer, Header, Skills, Testimonial, Work } from "./container";
+import { Navbar } from "./components";
+import "./App.scss";
+
 const App = () => {
   return (
-    <>
-      <h1>App</h1>
-      <p>anjay</p>
-    </>
+    <div className="app">
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
+    </div>
   );
 };
 
